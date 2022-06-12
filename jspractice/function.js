1f79322447ae5706ea856b4fc9c5c0ee1fa90246
tonito45 <somusartungi47@gmail.com>
2022-06-12 11:54:49 +0545

    calc design

---
+++ jspractice/function.js
@@ -0,0 +1,64 @@
+let f = 8;
+g = 5;
+function Add (x,y){
+    let result=0;
+    result= (x+y);
+    return result;
+};
+
+console.log (Add(f,g));
+ 
+
+let a = 20;
+b =10;
+function subtract (x,y){
+    let output = 0;
+    output = (x-y);
+    return output;
+}
+
+console.log (subtract(a,b));
+
+let c = "34";
+d ="23";
+function gun (x,y){
+    let get = 0;
+    get = (c*d);
+    return get;
+}
+
+console.log (gun(c,d));
+
+let e = 40;
+f = 4;
+function lap (c,d){
+    let cap = 0;
+    cap = (c/d);
+    return cap;
+}
+
+console.log (lap(e,f).toString());
+
+h = 50;
+i = 70;
+function hi (a,b){
+    let bi = 0;
+    bi = (b%a);
+    return bi;
+}
+console.log (hi(h,i));
+
+age = 19;
+function da (old){
+    let ad = (old>=18) ? "u are old":"you are young";
+    return ad;
+}
+console.log (da(age));
+
+let j = 89;
+k = 98;
+function compare (c,d){
+    let bus = (c==d);
+    return bus;
+}
+console.log (compare(j,k));
